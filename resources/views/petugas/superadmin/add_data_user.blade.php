ce9176179bd296e8384e3f83609f1f080b9a86cf
Cyntia Simamora <cyntiaevelin12@gmail.com>
2022-10-12 16:41:58 +0700

    perubahan form pasien

--- resources/views/petugas/superadmin/add_data_user.blade.php
+++ resources/views/petugas/superadmin/add_data_user.blade.php
@@ -5,7 +5,7 @@
 
 <div class="page-heading">
     <div class="page-title">
-        @section('judul', 'Add Data Petugas')
+        @section('judul', 'Tambah Data Petugas')
         @section('container')
         <div class="row">
             <div class="col-12 col-md-6 order-md-2 order-first">
@@ -69,7 +69,7 @@
                                     <div class="col-md-6 col-12">
                                         <div class="form-group">
                                             <label for="password">Password <b class="color-red">*</b></label>
-                                            <input type="password" name="password" id="password" class="form-control form-control-sm" minlength="12" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,}" title="Minimum 12 characters, at least one uppercase letter, one lowercase letter and one number (EXAMPLE : Passuser2022)" name="password" placeholder="Masukkan Password" required>
+                                            <input type="password" name="password" id="password" class="form-control form-control" minlength="12" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{12,}" title="Minimum 12 characters, at least one uppercase letter, one lowercase letter and one number (EXAMPLE : Passuser2022)" placeholder="Masukkan Password" required>
                                         </div>
                                     </div>
 
