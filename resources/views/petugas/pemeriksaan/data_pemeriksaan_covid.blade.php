60db967b5e552a20ed0451d8ee8f6e9f2e12c84a
hiski46 <hiski46@gmail.com>
2023-05-03 10:55:18 +0700

    perbaikan tampilan data pemeriksaan covid

--- resources/views/petugas/pemeriksaan/data_pemeriksaan_covid.blade.php
+++ resources/views/petugas/pemeriksaan/data_pemeriksaan_covid.blade.php
@@ -33,81 +33,83 @@
                 </li>
             </ul>
         </div>
-        <div class="card-body body-1 mt-3" style="display:none;">
-            @if (Session('message'))
-            <script>Swal.fire({ 
-                icon: "success", 
-                text: "{{Session('message')}}" }).then((result)=>{if (result.isConfirmed) { window.location.href = "{{ route('pemeriksaan.datapemeriksaancovid') }}" }})
-                </script>
-            @endif
-            <div class="table-responsive pt-2 pe-2">
-                <table class="table table-hover" id="table1">
-                    <thead>
-                        <tr>
-                            <th>Tanggal Pemeriksaan</th>
-                            <th>Nama Pasien</th>
-                            <th>Hasil Pemeriksaan</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($covid as $cov)
-                        <tr>
-                            <td><B>{{ Carbon\Carbon::parse($cov->created_at)->isoFormat('D MMMM Y') }}</B>
-                                <br>{{ Carbon\Carbon::parse($cov->created_at)->format('H:i:s') }}
-                            </td>
-                                <td>{{ $cov->pasien->nama_pasien }}</td>
-                                <td class="text-center">
-                                    @if ($cov->hasil_pemeriksaan==1)
-                                        <span class="badge bg-danger">Positif</span>
-                                    @else
-                                        <span class="badge bg-primary">Negatif</span>
-                                    @endif
+        <div class="card-body">
+            <div class="card-body body-1 mt-3" style="display:none;">
+                @if (Session('message'))
+                <script>Swal.fire({ 
+                    icon: "success", 
+                    text: "{{Session('message')}}" }).then((result)=>{if (result.isConfirmed) { window.location.href = "{{ route('pemeriksaan.datapemeriksaancovid') }}" }})
+                    </script>
+                @endif
+                <div class="table-responsive pt-2 pe-2">
+                    <table class="table table-hover" id="table1">
+                        <thead>
+                            <tr>
+                                <th>Tanggal Pemeriksaan</th>
+                                <th>Nama Pasien</th>
+                                <th>Hasil Pemeriksaan</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($covid as $cov)
+                            <tr>
+                                <td><B>{{ Carbon\Carbon::parse($cov->created_at)->isoFormat('D MMMM Y') }}</B>
+                                    <br>{{ Carbon\Carbon::parse($cov->created_at)->format('H:i:s') }}
                                 </td>
-                                <td class="text-center">
-                                    <div class="btn-group" role="group" aria-label="Basic outlined example">
-                                        <a href="/view/pemeriksaan/covid/{{ $cov->id }}" title="print Data" href="#" class="btn btn-outline-secondary"><i class="bi bi-eye-fill"></i></a>
-                                        <a href="/ubah/pemeriksaan/covid/{{ $cov->id }}" class="btn btn-outline-secondary" title="Edit"><i class="bi bi-pencil-square"></i></a>
-                                    </div>
-                                </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                                    <td>{{ $cov->pasien->nama_pasien }}</td>
+                                    <td class="text-center">
+                                        @if ($cov->hasil_pemeriksaan==1)
+                                            <span class="badge bg-danger">Positif</span>
+                                        @else
+                                            <span class="badge bg-primary">Negatif</span>
+                                        @endif
+                                    </td>
+                                    <td class="text-center">
+                                        <div class="btn-group" role="group" aria-label="Basic outlined example">
+                                            <a href="/view/pemeriksaan/covid/{{ $cov->id }}" title="print Data" href="#" class="btn btn-outline-secondary"><i class="bi bi-eye-fill"></i></a>
+                                            <a href="/ubah/pemeriksaan/covid/{{ $cov->id }}" class="btn btn-outline-secondary" title="Edit"><i class="bi bi-pencil-square"></i></a>
+                                        </div>
+                                    </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="card-body body-2 mt-3" style="display:none">
-
-            <div class="table-responsive pt-2 pe-2 ">
-                <table class="table table-hover" id="table2">
-                    <thead>
-                        <tr>
-                            <th>Tanggal Pemantauan</th>
-                            <th>Nama Pasien</th>
-                            <th>No. Kamar</th>
-                            <th>Tanggal Perjalanan</th>
-                            <th>Kota Asal/Kota Tujuan</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($pemantauan as $pantau)
-                        <tr>
-                                <td><B>{{ Carbon\Carbon::parse($pantau->tanggal_pemeriksaan)->isoFormat('D MMMM Y') }}</B></td>
-                                <td>{{ $pantau->pasien->nama_pasien }}</td>
-                                <td>{{ $pantau->no_kamar }}</td>
-                                <td>{{ $pantau->perjalanan }}</td>
-                                <td>{{ ($pantau->asal)??'- ' }}/{{ ($pantau->kota_tujuan)??' -' }}</td>
-                                <td class="text-center">
-                                    <div class="btn-group" role="group" aria-label="Basic outlined example">
-                                        <a href="/view/pemantauan/covid/{{ $pantau->id }}" title="View Data" href="#" class="btn btn-outline-secondary"><i class="bi bi-eye-fill"></i></a>
-                                        <a href="/ubah/pemantauan/covid/{{ $pantau->id }}" class="btn btn-outline-secondary" title="Edit"><i class="bi bi-pencil-square"></i></a>
-                                    </div>
-                                </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="card-body body-2 mt-3" style="display:none">
+    
+                <div class="table-responsive pt-2 pe-2 ">
+                    <table class="table table-hover" id="table2">
+                        <thead>
+                            <tr>
+                                <th>Tanggal Pemantauan</th>
+                                <th>Nama Pasien</th>
+                                <th>No. Kamar</th>
+                                <th>Tanggal Perjalanan</th>
+                                <th>Kota Asal/Kota Tujuan</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pemantauan as $pantau)
+                            <tr>
+                                    <td><B>{{ Carbon\Carbon::parse($pantau->tanggal_pemeriksaan)->isoFormat('D MMMM Y') }}</B></td>
+                                    <td>{{ $pantau->pasien->nama_pasien }}</td>
+                                    <td>{{ $pantau->no_kamar }}</td>
+                                    <td>{{ $pantau->perjalanan }}</td>
+                                    <td>{{ ($pantau->asal)??'- ' }}/{{ ($pantau->kota_tujuan)??' -' }}</td>
+                                    <td class="text-center">
+                                        <div class="btn-group" role="group" aria-label="Basic outlined example">
+                                            <a href="/view/pemantauan/covid/{{ $pantau->id }}" title="View Data" href="#" class="btn btn-outline-secondary"><i class="bi bi-eye-fill"></i></a>
+                                            <a href="/ubah/pemantauan/covid/{{ $pantau->id }}" class="btn btn-outline-secondary" title="Edit"><i class="bi bi-pencil-square"></i></a>
+                                        </div>
+                                    </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
